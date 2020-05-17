b2db8ecfc2144469c3295d29a85a7eae232fc1c6
Mitesh Modi <mitesh.modi003@gmail.com>
2020-05-16 21:58:18 -0400

    add a route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -14,6 +14,11 @@ module.exports = function(app) {
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
+
+  app.get("/", function(req, res) {
+    res.json(path.join(__dirname, "/../public/index.html"));
+  });
+
   app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/notes.html"));
   });
