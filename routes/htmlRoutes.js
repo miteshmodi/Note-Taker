a53baed9e2135c9600e64b7a98381b0814fee142
Mitesh Modi <mitesh.modi003@gmail.com>
2020-05-16 22:04:04 -0400

    removed a route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -15,10 +15,6 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("/", function(req, res) {
-    res.json(path.join(__dirname, "/../public/index.html"));
-  });
-
   app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/notes.html"));
   });
